e9973d4b47eab9f82e2057e9f8c1fa5dad146b9c
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-04-15 09:59:13 +1200

    checked, passed

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 /*const port = 3000 /*1024 -> 65535 - are good port numbers for dev, they are typically used in development, don't need to be an admin user with special privileges. Do not use const port =80*/
 const port = 3001
-const testRoute = require('./routes/myTestRoute')
+// const testRoute = require('./routes/myTestRoute')
 
 
 //swagger
@@ -23,7 +23,7 @@ app.use('/', express.static('public'))
 app.use(express.json())
 
 //create a route to a router -calling caluclator route
-app.use('/my-test-route', testRoute)   
+// app.use('/my-test-route', testRoute)
 app.use('/calculator', calculatorRoute)
 
 /*app.get('/test', (req, res) => {
@@ -31,7 +31,7 @@ res.send('Hello World! 123 456')
 }) */
 
 //start the server
-app.listen(port, () => {  
+app.listen(port, () => {
 console.log(`Example app listening at http://localhost:${port}`)
 })
 
@@ -41,22 +41,25 @@ app.use('/calculator',calculatorRoute)
 app.get('/', (req, res) => {
     res.send('Hello World2!')
     })
-    
-    app.listen(port2, () => {  
+
+    app.listen(port2, () => {
     console.log(`Example app
     listening at
     http://localhost:${port2}`)
     }) */
-    
-/*create new endpoints with port number - copy and paste. Then do 'npm start' in terminal and should show new port number in terminal. Ctrl and click - 
+
+/*create new endpoints with port number - copy and paste. Then do 'npm start' in terminal and should show new port number in terminal. Ctrl and click -
 should show new message */
 
 /*app.get('/', (req, res) => {
 res.send('Hello World!')
 })
 
-app.listen(port, () => {  
+app.listen(port, () => {
 console.log(`Example app
 listening at
 http://localhost:${port}`)
 }) */
+
+//Great work, Janine! you have completed the module 5 exercise 6.
+//you were able to create an api endpoint to your api documentation using swagger.
